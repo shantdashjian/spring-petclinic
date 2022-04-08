0e2cc4ce63664a100cccb6ac0ee4d4094afa0f17
Shant Dashjian <shantgdashjian@gmail.com>
2022-04-07 20:07:39 -0700

    Add email sending step

--- .vscode/pipeline.groovy
+++ .vscode/pipeline.groovy
@@ -13,14 +13,18 @@ pipeline {
         }
         stage('Build') {
             steps {
-                sh "./mvnw clean package"
+                sh './mvnw clean package'
             }
 
             post {
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
-                }
+                // }
+
+                // changed {
+                    emailext attachLog: true, body: ' Please go to ${BUILD_URL} and verify the build.', compressLog: true, subject: 'Job \'${JOB_NAME}\' (${BUILD_NUMBER}) is waiting for input', to: 'shantgdashjian@gmail.com'                
+                    }
             }
         }
     }
