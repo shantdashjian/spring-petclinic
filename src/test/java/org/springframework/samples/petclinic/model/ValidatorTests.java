44e4ee12e4f47005b745ee97c8247254120e33c2
Shant Dashjian <shantgdashjian@gmail.com>
2022-04-07 20:14:57 -0700

    Fix test

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -44,7 +44,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("Not empty");
+		person.setFirstName("");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
